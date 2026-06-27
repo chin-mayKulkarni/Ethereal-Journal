3bd09b2d407fb30908ab39695e6c58f425a4da08
Chinmay Kulkarni <chinmayrk01@gmail.com>
2026-06-27 19:12:04 +0530

    updated gitignore file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.diary.ai"
         minSdk = 26
         targetSdk = 37
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
